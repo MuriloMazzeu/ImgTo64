d5554e731cb0ed80ebb7f50f835aba76794381b4
Murilo Mazzeu <mumazzeu@live.com>
2018-04-30 22:32:55 -0300

    Finished a minimal software implementation

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ImgTo64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtImagePath = new System.Windows.Forms.TextBox();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtImagePath);
+            this.groupBox1.Controls.Add(this.btnImage);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(459, 236);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Convert";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(7, 67);
+            this.txtResult.MaxLength = 2147483647;
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(446, 163);
+            this.txtResult.TabIndex = 3;
+            this.txtResult.Click += new System.EventHandler(this.txtResult_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // txtImagePath
+            // 
+            this.txtImagePath.Enabled = false;
+            this.txtImagePath.Location = new System.Drawing.Point(100, 22);
+            this.txtImagePath.Name = "txtImagePath";
+            this.txtImagePath.ReadOnly = true;
+            this.txtImagePath.Size = new System.Drawing.Size(353, 20);
+            this.txtImagePath.TabIndex = 1;
+            // 
+            // btnImage
+            // 
+            this.btnImage.Location = new System.Drawing.Point(7, 20);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(87, 23);
+            this.btnImage.TabIndex = 0;
+            this.btnImage.Text = "Select image";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.btnImage_Click);
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.Filter = "Image Files(*.PNG;*.BMP;*.JPG;*.JPEG;*.GIF)|*.PNG;*.BMP;*.JPG;*.JPEG;*.GIF";
+            this.fileDialog.Title = "Image to convert";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ImgTo64";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtImagePath;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgTo64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImage_Click(object sender, EventArgs e)
+        {
+            if(fileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                txtImagePath.Text = fileDialog.FileName;
+                using (var image = Image.FromFile(fileDialog.FileName))
+                {
+                    using (var m = new MemoryStream())
+                    {
+                        var format = image.RawFormat;
+
+                        image.Save(m, format);
+                        var imageBytes = m.ToArray();
+                        var base64String = Convert.ToBase64String(imageBytes);                        
+                        var codec = ImageCodecInfo.GetImageDecoders().First(c => c.FormatID == format.Guid);
+
+                        txtResult.Text = $"data:{codec.MimeType};base64,{base64String}";
+                    }
+                }
+            }
+        }
+
+        private void txtResult_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtResult.Text);
+            txtResult.SelectAll();
+        }
+    }
+}
